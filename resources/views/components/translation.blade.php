278043f6bfb369e0ef45510644b0ba2bb56d8a62
Fady Mondy <EngFadyMondy@gmail.com>
2023-09-05 17:49:06 +0300

    add translation component

--- resources/views/components/translation.blade.php
+++ resources/views/components/translation.blade.php
@@ -1,4 +1,5 @@
 <x-splade-data default="{ {{$name.'_tomato_translations_en'}}: form.{{$name}}?.en, {{$name.'_tomato_translations_ar'}}: form.{{$name}}?.ar }">
+    @if($type === 'text')
     <label class="block">
         <span class="block text-sm font-medium leading-6 text-gray-950 dark:text-white">{{$label}}</span>
         <div class="flex rounded-lg border border-gray-300 dark:border-gray-700 shadow-sm dark:text-white">
@@ -20,4 +21,27 @@
 ])->implode(' ') }}" v-model="data.{{$name.'_tomato_translations_en'}}" @input="form.{{$name.'_tomato_translations_en'}} = data.{{$name.'_tomato_translations_en'}}" type="text" placeholder="{{$label . ' '. __('EN')}}" />
         </div>
     </label>
+    @elseif($type === 'textarea')
+        <label class="block">
+            <span class="block text-sm font-medium leading-6 text-gray-950 dark:text-white">{{$label}}</span>
+            <div class="flex rounded-lg border border-gray-300 dark:border-gray-700 shadow-sm dark:text-white">
+                <textarea class="{{ collect([
+    "fi-input block w-full border-none bg-transparent py-1.5 text-base text-gray-950 outline-none transition",
+    "duration-75 placeholder:text-gray-400 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)]",
+    "disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400",
+    "dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm",
+    "sm:leading-6 ps-3 pe-3 focus:ring-2 ring-primary-500 focus:ring-2 focus:ring-primary-500 rounded-lg"
+])->implode(' ') }}" v-model="data.{{$name.'_tomato_translations_ar'}}" @input="form.{{$name.'_tomato_translations_ar'}} = data.{{$name.'_tomato_translations_ar'}}"  placeholder="{{$label . ' '. __('AR')}}" />
+            </div>
+            <div class="flex rounded-lg mt-4 border border-gray-300 dark:border-gray-700 shadow-sm dark:text-white">
+                <textarea class="{{ collect([
+    "fi-input block w-full border-none bg-transparent py-1.5 text-base text-gray-950 outline-none transition",
+    "duration-75 placeholder:text-gray-400 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)]",
+    "disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400",
+    "dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm",
+    "sm:leading-6 ps-3 pe-3 focus:ring-2 ring-primary-500 focus:ring-2 focus:ring-primary-500 rounded-lg"
+])->implode(' ') }}" v-model="data.{{$name.'_tomato_translations_en'}}" @input="form.{{$name.'_tomato_translations_en'}} = data.{{$name.'_tomato_translations_en'}}"  placeholder="{{$label . ' '. __('EN')}}" />
+            </div>
+        </label>
+   @endif
 </x-splade-data>
