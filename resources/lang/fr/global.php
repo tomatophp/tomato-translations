333495842484074e3ae7ecb6c2dc4c075e635a2c
Martin Lechêne <martin@lechene.be>
2023-10-11 13:02:53 +0200

    Create lang/fr/global

---
+++ resources/lang/fr/global.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    "title" => "Traductions",
+    "single" => "Traduction",
+    "group" => "Outils",
+    "excel" => "Excel",
+    "scan" => "Scan",
+    "auto" => "Auto",
+    "import" => "Import",
+    "export" => "Export",
+    "ar" => "Arabe",
+    "en" => "Anglais",
+    "fr" => "Français"
+    "id" => "ID",
+    "key" => "Key",
+    "translate" => "Traduire",
+    "message" => [
+        "deleted" => "Enregistrement de traduction supprimé avec succès",
+        "update" => "L'enregistrement de traduction a été mis à jour avec succès",
+        "import" => "L'enregistrement de traduction a été importé avec succès",
+        "scan" => "L'enregistrement de traduction a été scanné avec succès",
+        "auto" => "L'enregistrement de traduction a été traduit automatiquement avec succès",
+        "auto_error" => "L'enregistrement de traduction a une erreur lors de la traduction automatique",
+    ],
+];
