73311751df97766a359fc50abaa74af74f5118e8
Martin Lechêne <martin@lechene.be>
2023-10-11 13:05:04 +0200

    Add div for input and textarea for FR

--- resources/views/components/translation.blade.php
+++ resources/views/components/translation.blade.php
@@ -1,4 +1,4 @@
-<x-splade-data default="{ {{$name.'_tomato_translations_en'}}: form.{{$name}}?.en, {{$name.'_tomato_translations_ar'}}: form.{{$name}}?.ar }">
+<x-splade-data default="{ {{$name.'_tomato_translations_en'}}: form.{{$name}}?.en, {{$name.'_tomato_translations_ar'}}: form.{{$name}}?.ar, {{$name.'_tomato_translations_fr'}}: form.{{$name}}?.fr }">
     @if($type === 'text')
     <label class="block">
         <span class="block text-sm font-medium leading-6 text-gray-950 dark:text-white">{{$label}}</span>
@@ -19,6 +19,16 @@
     "dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm",
     "sm:leading-6 ps-3 pe-3 focus:ring-2 ring-primary-500 focus:ring-2 focus:ring-primary-500 rounded-lg"
 ])->implode(' ') }}" v-model="data.{{$name.'_tomato_translations_en'}}" @input="form.{{$name.'_tomato_translations_en'}} = data.{{$name.'_tomato_translations_en'}}" type="text" placeholder="{{$label . ' '. __('EN')}}" />
+        </div>
+        
+        <div class="flex rounded-lg mt-4 border border-gray-300 dark:border-gray-700 shadow-sm dark:text-white">
+            <input class="{{ collect([
+    "fi-input block w-full border-none bg-transparent py-1.5 text-base text-gray-950 outline-none transition",
+    "duration-75 placeholder:text-gray-400 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)]",
+    "disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400",
+    "dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm",
+    "sm:leading-6 ps-3 pe-3 focus:ring-2 ring-primary-500 focus:ring-2 focus:ring-primary-500 rounded-lg"
+])->implode(' ') }}" v-model="data.{{$name.'_tomato_translations_fr'}}" @input="form.{{$name.'_tomato_translationsfr'}} = data.{{$name.'_tomato_translations_fr'}}" type="text" placeholder="{{$label . ' '. __('FR')}}" />
         </div>
     </label>
     @elseif($type === 'textarea')
@@ -41,6 +51,15 @@
     "dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm",
     "sm:leading-6 ps-3 pe-3 focus:ring-2 ring-primary-500 focus:ring-2 focus:ring-primary-500 rounded-lg"
 ])->implode(' ') }}" v-model="data.{{$name.'_tomato_translations_en'}}" @input="form.{{$name.'_tomato_translations_en'}} = data.{{$name.'_tomato_translations_en'}}"  placeholder="{{$label . ' '. __('EN')}}" />
+            </div>
+            <div class="flex rounded-lg mt-4 border border-gray-300 dark:border-gray-700 shadow-sm dark:text-white">
+                <textarea class="{{ collect([
+    "fi-input block w-full border-none bg-transparent py-1.5 text-base text-gray-950 outline-none transition",
+    "duration-75 placeholder:text-gray-400 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)]",
+    "disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400",
+    "dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm",
+    "sm:leading-6 ps-3 pe-3 focus:ring-2 ring-primary-500 focus:ring-2 focus:ring-primary-500 rounded-lg"
+])->implode(' ') }}" v-model="data.{{$name.'_tomato_translations_fr'}}" @input="form.{{$name.'_tomato_translations_fr'}} = data.{{$name.'_tomato_translations_fr'}}"  placeholder="{{$label . ' '. __('FR')}}" />
             </div>
         </label>
    @endif
